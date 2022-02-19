ad5928661b497a67414e70e2f6362263c983583f
Samuel Yuen <samuel.s.yuen@gmail.com>
2022-02-19 16:58:09 -0500

    + added router and routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Portfolio} from "./components/Portfolio";
+import {About} from "./components/About";
+import {Comparison} from "./components/Comparison";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<App/>}/>
+            <Route path={"/about"} element={<About/>}/>
+            <Route path={"/comparison"} element={<Comparison/>}/>
+            <Route path={"/portfolio"} element={<Portfolio/>}/>
+        </Routes>
+
+    </BrowserRouter>,
+    document.getElementById('root')
 )
