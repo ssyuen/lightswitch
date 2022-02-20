c41ebc46c5608f2cf297737f253173581884bfa7
Samuel Yuen <samuel.s.yuen@gmail.com>
2022-02-20 03:37:31 -0500

    + updated home link to be logo and added risks

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import {Menu} from "antd";
 import {Link, useLocation} from "react-router-dom";
 import {Header} from "antd/es/layout/layout";
+import {BulbOutlined} from "@ant-design/icons";
 
 export const Navbar = () => {
     return (
@@ -9,10 +10,14 @@ export const Navbar = () => {
                 <div className="logo"/>
                 <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[useLocation().pathname]}>
                     <Menu.Item key={"/"}>
-                        <Link to={"/"}>Home</Link>
+                        <Link to={"/"}><BulbOutlined/>
+                            lightswitch</Link>
                     </Menu.Item>
                     <Menu.Item key={"/comparison"}>
                         <Link to={"/comparison"}>Comparison</Link>
+                    </Menu.Item>
+                    <Menu.Item key={"/risks"}>
+                        <Link to={"/risks"}>Risks</Link>
                     </Menu.Item>
                     <Menu.Item key={"/portfolio"}>
                         <Link to={"/portfolio"}>Portfolio</Link>
